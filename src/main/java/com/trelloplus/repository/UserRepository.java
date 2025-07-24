e595425e54fb5a6bf615375c2717e0e0571c349c
shafique <shafiquelhr0@gmail.com>
2025-07-24 21:09:19 +0500

    microservices

--- src/main/java/com/trelloplus/repository/UserRepository.java
+++ src/main/java/com/trelloplus/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.trelloplus.repository;
 
+import com.trelloplus.dto.UserDto;
 import com.trelloplus.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+    UserDto findByEmail(String email);
+}
--- src/main/java/com/trelloplus/service/impl/BoardServiceImpl.java
+++ src/main/java/com/trelloplus/service/impl/BoardServiceImpl.java
@@ -4,6 +4,7 @@ import com.trelloplus.exception.BoardNotFoundException;
 import com.trelloplus.model.Board;
 import com.trelloplus.repository.BoardRepository;
 import com.trelloplus.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,6 +14,7 @@ public class BoardServiceImpl implements BoardService {
 
     private final BoardRepository boardRepository;
 
+    @Autowired
     public BoardServiceImpl(BoardRepository boardRepository) {
         this.boardRepository = boardRepository;
     }
